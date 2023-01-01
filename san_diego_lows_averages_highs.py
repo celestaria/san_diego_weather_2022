f9d06ae119924b8a5ec95c46429bf1f9b2d16ae9
Frances Cornwall <frances_c@cox.net>
2023-01-01 13:08:57 -0800

    Edited one of the charts to match the other ones

--- san_diego_lows_averages_highs.py
+++ san_diego_lows_averages_highs.py
@@ -1,5 +1,6 @@
 # Import libraries and dependencies
 from datetime import datetime
+import matplotlib
 import matplotlib.pyplot as plt
 from meteostat import Point, Daily
 
@@ -13,14 +14,11 @@ san_diego = Point(32.7157, 117.1611, 62)
 # Get daily data
 data = Daily(san_diego, start, end)
 data = data.fetch()
+plt.style.use('seaborn')
 
 # Plot line chart showing minimum, average, and maximum temperature
 ax = data.plot(
     y=['tavg', 'tmin', 'tmax'],
-    lw=2,
-    colormap='jet',
-    marker='.',
-    markersize=10,
     figsize=(10, 7.5)
     )
 ax.set_title("Daily average, low, and high temperatures - 2022 San Diego, CA", fontsize=20)
